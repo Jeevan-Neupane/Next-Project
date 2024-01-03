393029b4cc17d7d6976fd45189cb9d18ffabb799
Jeevan-Neupane <jeevan.neupane003@gmail.com>
2024-01-03 19:50:42 +0545

    Blog page is made

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,18 +1,53 @@
-import React from "react";
 import Image from "next/image";
 import styles from "./about.module.css";
-function AboutPage() {
+
+export const metadata = {
+  title: "About Page",
+  description: "About description",
+};
+
+
+const AboutPage = () => {
+
+  // console.log("lets check where it works")
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subtitle}>About Agency</h2>
+        <h1 className={styles.title}>
+          We create digital ideas that are bigger, bolder, braver and better.
+        </h1>
+        <p className={styles.desc}>
+          We create digital ideas that are bigger, bolder, braver and better. We
+          believe in good ideas flexibility and precission We’re world’s Our
+          Special Team best consulting & finance solution provider. Wide range
+          of web and software development services.
+        </p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+        </div>
+      </div>
       <div className={styles.imgContainer}>
         <Image
-          src='https://images.unsplash.com/photo-1586516612497-350f5995c33d?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGR1YSUyMGxpcGF8ZW58MHx8MHx8fDA%3D'
-          alt='about image'
+          src="/about.png"
+          alt="About Image"
           fill
+          className={styles.img}
         />
       </div>
     </div>
   );
-}
+};
 
 export default AboutPage;
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import styles from "./blog.module.css";
+import PostCard from "@/components/postCard/PostCard";
 
 function BlogPage() {
   return (
-    <div>BlogPage</div>
-  )
+    <div className={styles.container}>
+      <div className={styles.post}>
+
+      <PostCard />
+
+
+      </div>
+      <div className={styles.post}>
+
+      <PostCard />
+
+
+      </div>
+      <div className={styles.post}>
+
+      <PostCard />
+
+
+      </div>
+      <div className={styles.post}>
+
+      <PostCard />
+
+
+      </div>
+      <div className={styles.post}>
+
+      <PostCard />
+
+
+      </div>
+    </div>
+  );
 }
 
-export default BlogPage
+export default BlogPage;
